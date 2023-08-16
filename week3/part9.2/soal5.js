f929f68a970973461abc4117896d2e3a8eafd964
aldoLorenzio <aldolorenzio@gmail.com>
2023-08-16 17:10:10 +0700

    soal 4 belum done yak, nnti direvisi ulang

---
+++ week3/part9.2/soal5.js
@@ -0,0 +1,22 @@
+//sistem ubah hurufnya misal huruf a diubah menjadi b, c menjadi d, b menjadi c, z menjadi a
+//intinya ubah huruf menjadi huruf setelahnya
+function ubahHuruf(kata) {
+    // you can only write your code here!
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    let hasil = ''
+    for(let i = 0; i <= kata.length-1; i++){
+        for(let j = 0; j <= alphabet.length-1;j++){
+            if(kata[i] == alphabet[j]){
+                hasil+= alphabet[j+1]
+            }
+        }
+    }
+    return hasil;
+  }
+  
+  // TEST CASES
+  console.log(ubahHuruf('wow')); // xpx
+  console.log(ubahHuruf('developer')); // efwfmpqfs
+  console.log(ubahHuruf('javascript')); // kbwbtdsjqu
+  console.log(ubahHuruf('keren')); // lfsfo
+  console.log(ubahHuruf('semangat')); // tfnbohbu
